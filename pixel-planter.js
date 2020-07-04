c9f2c69fce185bd29d7b6e7b2c118222dcd2cc31
Brandon Gioggia <bgioggia@gmail.com>
2020-07-04 16:13:38 -0400

    Code Cleanup
    Added more comments
    
    changed the if/else if/else for determining branch side and angle to 4 lines of formulas.

--- pixel-planter.js
+++ pixel-planter.js
@@ -1,23 +1,29 @@
-function testTrunk(ctx) {
+function testTrunk() {
+
+    //get canvas info & all that jazz
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
 
+    //set width as random value within given range
     var minWidth = 10;
     var maxWidth = 24;
-
     var width = getRandomInt(maxWidth - minWidth) + minWidth;
 
     //ensures width is even
     width = width + (width % 2)
 
+    //set height as random value within given range
     var minHeight = 60;
     var maxheight = 100;
-
     var height = getRandomInt(maxheight - minHeight) + minHeight;
+
+    //ensures height is even
     height = height + (height % 2)
 
-    var totalPixels = height * width
+    //set color to brown
     ctx.strokeStyle = "#5a3d17";
+
+    //draw trunk
     for (var i = 0; i < height; i++) {
         ctx.beginPath();
         ctx.moveTo(64 - (width / 2), 128 - i);
@@ -25,57 +31,45 @@ function testTrunk(ctx) {
         ctx.stroke()
     }
 
+    //for making random choices with binary options.
+    var mult = [-1, 1];
+
+    //set number of branches as random value within given range
     var maxBranches = 40;
     var minBranches = 5;
-    var branches = getRandomInt(maxBranches - minBranches);
+    var branches = getRandomInt(maxBranches - minBranches) + minBranches;
 
+    //draw branches on tree
     for (var j = 0; j < branches; j++) {
+
+        //ensures branch color is brown
         ctx.strokeStyle = "#5a3d17";
 
-        //left = 1, right = 2
+        //left = 0, right = 1
         var side = getRandomInt(2);
-        //up = 1, down = 2
+
+        //up = 0, down = 1
         var angle = getRandomInt(2);
 
-        //clean this up with a +-1 later
-        var startx;
-        var endx;
-        var starty;
-        var endy;
-        if (side === 1) {
-            startx = 64;
-            endx = 55 - getRandomInt(30);
-            starty = getRandomInt(height / 2);
-        } else {
-            startx = 65;
-            endx = 74 + getRandomInt(30);
-        }
-        if (angle === 1) {
-            starty = getRandomInt((2 * height) / 3) + (height / 3);
-            endy = getRandomInt(height / 10) + starty;
-        } else {
-            starty = getRandomInt(height / 2) + (height / 2);
-            endy = starty - getRandomInt(height / 5);
-        }
+        //randomly generate length of branch for generated side and angle
+        var startx = 64 + (side % 2);
+        var endx = startx + (mult[side] * getRandomInt(30));
+        var starty = getRandomInt((2 * height) / 3) + (height / 3);
+        var endy = starty + (mult[angle] * getRandomInt(height / 5));
+
+        //draw banch
         ctx.beginPath();
         ctx.moveTo(startx, 128 - (starty));
         ctx.lineTo(endx, 128 - (endy));
         ctx.stroke();
 
-        //
+        //draw vine at the end of branch
         addVine(22, ctx, endx, endy);
     }
-
-    //  ctx.beginPath();
-    // ctx.moveTo(100, 77);
-    //  ctx.lineTo(80, 98);
-    //   ctx.strokeStyle = "#234705";
-    //   ctx.stroke()
-
 }
 
+//function for drawing a single green vine
 function addVine(vineLength, ctx, startX, startY) {
-
     for (var i = 0; i < vineLength; i++) {
         ctx.beginPath();
         ctx.moveTo(startX + ((-1 * (i % 2)) * (Math.sin(i) / 5)), 128 - startY + i);
@@ -85,6 +79,7 @@ function addVine(vineLength, ctx, startX, startY) {
     }
 }
 
+//function for getting a random int from 0<max
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
